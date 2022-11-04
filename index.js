b4ffaa34a32e6e027bb93c96ec7c0086861e4a07
Didarhalla <didarhallayev255@gmail.com>
2022-11-04 20:04:20 +0300

    upgrade js shortened Objects, when click heart going red color first commit

--- index.js
+++ index.js
@@ -1,129 +1,24 @@
 'use strict'
 
+let globalKart = {}
+let image = ['https://a.lmcdn.ru/img236x341/R/T/RTLABM929601_17356797_1_v1.jpg', 'https://a.lmcdn.ru/img236x341/R/T/RTLABQ520101_17717054_1_v1.jpg', 'https://a.lmcdn.ru/img236x341/A/D/AD093AUMOKG9_13279634_1_v1.jpg', 'https://a.lmcdn.ru/img236x341/R/T/RTLACB638801_18413347_1_v1_2x.jpg', 'https://a.lmcdn.ru/img236x341/R/T/RTLABX723401_17973506_1_v1.jpg', 'https://a.lmcdn.ru/img236x341/R/T/RTLAAX263202_17963357_1_v1.jpg', 'https://a.lmcdn.ru/img236x341/R/T/RTLABP405301_17739711_1_v1_2x.jpg', 'https://a.lmcdn.ru/img236x341/R/T/RTLABX877701_17875917_1_v3.jpg', 'https://a.lmcdn.ru/img236x341/R/T/RTLABK472901_17039311_1_v2.jpg', 'https://a.lmcdn.ru/img236x341/R/T/RTLABY543901_18046135_1_v1.jpg', 'https://a.lmcdn.ru/img236x341/R/T/RTLABP912201_17719185_1_v1_2x.jpg', 'https://a.lmcdn.ru/img236x341/R/T/RTLACA047201_18082402_1_v1_2x.jpg', 'https://a.lmcdn.ru/img236x341/R/T/RTLABM929601_17356797_1_v1.jpg', 'https://a.lmcdn.ru/img236x341/R/T/RTLABQ520101_17717054_1_v1.jpg', 'https://a.lmcdn.ru/img236x341/R/T/RTLAAX263202_17963357_1_v1.jpg', 'https://a.lmcdn.ru/img236x341/R/T/RTLABX723401_17973506_1_v1.jpg', 'https://a.lmcdn.ru/img236x341/A/D/AD093AUMOKG9_13279634_1_v1.jpg', 'https://a.lmcdn.ru/img236x341/R/T/RTLABK472901_17039311_1_v2.jpg', 'https://a.lmcdn.ru/img236x341/R/T/RTLABP912201_17719185_1_v1_2x.jpg', 'https://a.lmcdn.ru/img236x341/R/T/RTLABY543901_18046135_1_v1.jpg']
+let type = ['sneakers', 'sneakers', 'sneakers', 'sneakers', 'sneakers', 'sneakers', 'sneakers', 'sneakers', 'sneakers', 'sneakers', 'sneakers', 'sneakers', 'sneakers', 'sneakers', 'sneakers', 'sneakers', 'sneakers', 'sneakers', 'sneakers', 'sneakers']
+let name = ['adidas', 'adidas', 'adidas', 'adidas', 'adidas', 'adidas', 'adidas', 'adidas', 'adidas', 'adidas', 'adidas', 'adidas', 'adidas', 'adidas', 'adidas', 'adidas', 'adidas', 'adidas', 'adidas', 'adidas']
+let price = ['79$', '94$', '88$', '99$', '79$', '94$', '88$', '99$', '79$', '94$', '88$', '99$', '79$', '94$', '88$', '79$', '94$', '88$', '99$', '99$']
 
-let sneakersInformation1 = {
-    image: 'https://a.lmcdn.ru/img236x341/R/T/RTLABM929601_17356797_1_v1.jpg',
-    type: 'sneakers',
-    name: 'adidas',
-    price: '79$',
-}
-let sneakersInformation2 = {
-    image: 'https://a.lmcdn.ru/img236x341/R/T/RTLABQ520101_17717054_1_v1.jpg',
-    type: 'sneakers',
-    name: 'adidas',
-    price: '94$',
-}
-let sneakersInformation3 = {
-    image: 'https://a.lmcdn.ru/img236x341/A/D/AD093AUMOKG9_13279634_1_v1.jpg',
-    type: 'sneakers',
-    name: 'adidas',
-    price: '88$',
-}
-let sneakersInformation4 = {
-    image: 'https://a.lmcdn.ru/img236x341/R/T/RTLACB638801_18413347_1_v1_2x.jpg',
-    type: 'sneakers',
-    name: 'adidas',
-    price: '99$',
-}
-let sneakersInformation5 = {
-    image: 'https://a.lmcdn.ru/img236x341/R/T/RTLABX723401_17973506_1_v1.jpg',
-    type: 'sneakers',
-    name: 'adidas',
-    price: '79$',
-}
-let sneakersInformation6 = {
-    image: 'https://a.lmcdn.ru/img236x341/R/T/RTLAAX263202_17963357_1_v1.jpg',
-    type: 'sneakers',
-    name: 'adidas',
-    price: '94$',
-}
-let sneakersInformation7 = {
-    image: 'https://a.lmcdn.ru/img236x341/R/T/RTLABP405301_17739711_1_v1_2x.jpg',
-    type: 'sneakers',
-    name: 'adidas',
-    price: '88$',
-}
-let sneakersInformation8 = {
-    image: 'https://a.lmcdn.ru/img236x341/R/T/RTLABX877701_17875917_1_v3.jpg',
-    type: 'sneakers',
-    name: 'adidas',
-    price: '99$',
-}
-let sneakersInformation9 = {
-    image: 'https://a.lmcdn.ru/img236x341/R/T/RTLABK472901_17039311_1_v2.jpg',
-    type: 'sneakers',
-    name: 'adidas',
-    price: '79$',
-}
-let sneakersInformation10 = {
-    image: 'https://a.lmcdn.ru/img236x341/R/T/RTLABY543901_18046135_1_v1.jpg',
-    type: 'sneakers',
-    name: 'adidas',
-    price: '94$',
-}
-let sneakersInformation11 = {
-    image: 'https://a.lmcdn.ru/img236x341/R/T/RTLABP912201_17719185_1_v1_2x.jpg',
-    type: 'sneakers',
-    name: 'adidas',
-    price: '88$',
-}
-let sneakersInformation12 = {
-    image: 'https://a.lmcdn.ru/img236x341/R/T/RTLACA047201_18082402_1_v1_2x.jpg',
-    type: 'sneakers',
-    name: 'adidas',
-    price: '99$',
-}
-let sneakersInformation13 = {
-    image: 'https://a.lmcdn.ru/img236x341/R/T/RTLABM929601_17356797_1_v1.jpg',
-    type: 'sneakers',
-    name: 'adidas',
-    price: '79$',
-}
-let sneakersInformation14 = {
-    image: 'https://a.lmcdn.ru/img236x341/R/T/RTLABQ520101_17717054_1_v1.jpg',
-    type: 'sneakers',
-    name: 'adidas',
-    price: '94$',
-}
-let sneakersInformation15 = {
-    image: 'https://a.lmcdn.ru/img236x341/R/T/RTLAAX263202_17963357_1_v1.jpg',
-    type: 'sneakers',
-    name: 'adidas',
-    price: '88$',
-}
-let sneakersInformation16 = {
-    image: 'https://a.lmcdn.ru/img236x341/R/T/RTLABX723401_17973506_1_v1.jpg',
-    type: 'sneakers',
-    name: 'adidas',
-    price: '79$',
-}
-let sneakersInformation17 = {
-    image: 'https://a.lmcdn.ru/img236x341/A/D/AD093AUMOKG9_13279634_1_v1.jpg',
-    type: 'sneakers',
-    name: 'adidas',
-    price: '94$',
-}
-let sneakersInformation18 = {
-    image: 'https://a.lmcdn.ru/img236x341/R/T/RTLABK472901_17039311_1_v2.jpg',
-    type: 'sneakers',
-    name: 'adidas',
-    price: '88$',
-}
-let sneakersInformation19 = {
-    image: 'https://a.lmcdn.ru/img236x341/R/T/RTLABP912201_17719185_1_v1_2x.jpg',
-    type: 'sneakers',
-    name: 'adidas',
-    price: '99$',
-}
-let sneakersInformation20 = {
-    image: 'https://a.lmcdn.ru/img236x341/R/T/RTLABY543901_18046135_1_v1.jpg',
-    type: 'sneakers',
-    name: 'adidas',
-    price: '99$',
-}
+price.forEach((v, i) => {
+    globalKart['sneakersInformation' + String(i + 1)] = {
+        image: image[i],
+        type: type[i],
+        name: name[i],
+        price: price[i],
+    }
+})
 
-let massAllSneakersInfo = [sneakersInformation1, sneakersInformation2, sneakersInformation3, sneakersInformation4, sneakersInformation5, sneakersInformation6, sneakersInformation7, sneakersInformation8, sneakersInformation9, sneakersInformation10, sneakersInformation11, sneakersInformation12, sneakersInformation13, sneakersInformation14, sneakersInformation15, sneakersInformation16, sneakersInformation17, sneakersInformation18, sneakersInformation19, sneakersInformation20]
+let massAllSneakersInfo =  Object.values(globalKart)
 
+console.log(massAllSneakersInfo);
+// console.log(Object.keys(massAllSneakersInfo));
 
 let newMass = massAllSneakersInfo.forEach((sneaker) => {
     const divContainer = document.getElementById('container')
@@ -136,9 +31,6 @@ let newMass = massAllSneakersInfo.forEach((sneaker) => {
     const pName = document.createElement('p')
     const pType = document.createElement('p')
 
-    divContainer.append(divDad)
-
-    divContainer.append(divDad)
     divContainer.style.display = 'flex'
     divContainer.style.justifyContent = 'center'
     divContainer.style.marginTop = '70px'
