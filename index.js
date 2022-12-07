d840e84e78d1211fa226bb03ba46e77d8d47a8df
Didarhalla <didarhallayev255@gmail.com>
2022-12-07 19:52:20 +0300

    when clicked, an array of objects is displayed in the console

--- index.js
+++ index.js
@@ -1,23 +1,131 @@
 'use strict'
 
-let globalKart = {}
-let image = ['https://a.lmcdn.ru/img236x341/R/T/RTLABM929601_17356797_1_v1.jpg', 'https://a.lmcdn.ru/img236x341/R/T/RTLABQ520101_17717054_1_v1.jpg', 'https://a.lmcdn.ru/img236x341/A/D/AD093AUMOKG9_13279634_1_v1.jpg', 'https://a.lmcdn.ru/img236x341/R/T/RTLACB638801_18413347_1_v1_2x.jpg', 'https://a.lmcdn.ru/img236x341/R/T/RTLABX723401_17973506_1_v1.jpg', 'https://a.lmcdn.ru/img236x341/R/T/RTLAAX263202_17963357_1_v1.jpg', 'https://a.lmcdn.ru/img236x341/R/T/RTLABP405301_17739711_1_v1_2x.jpg', 'https://a.lmcdn.ru/img236x341/R/T/RTLABX877701_17875917_1_v3.jpg', 'https://a.lmcdn.ru/img236x341/R/T/RTLABK472901_17039311_1_v2.jpg', 'https://a.lmcdn.ru/img236x341/R/T/RTLABY543901_18046135_1_v1.jpg', 'https://a.lmcdn.ru/img236x341/R/T/RTLABP912201_17719185_1_v1_2x.jpg', 'https://a.lmcdn.ru/img236x341/R/T/RTLACA047201_18082402_1_v1_2x.jpg', 'https://a.lmcdn.ru/img236x341/R/T/RTLABM929601_17356797_1_v1.jpg', 'https://a.lmcdn.ru/img236x341/R/T/RTLABQ520101_17717054_1_v1.jpg', 'https://a.lmcdn.ru/img236x341/R/T/RTLAAX263202_17963357_1_v1.jpg', 'https://a.lmcdn.ru/img236x341/R/T/RTLABX723401_17973506_1_v1.jpg', 'https://a.lmcdn.ru/img236x341/A/D/AD093AUMOKG9_13279634_1_v1.jpg', 'https://a.lmcdn.ru/img236x341/R/T/RTLABK472901_17039311_1_v2.jpg', 'https://a.lmcdn.ru/img236x341/R/T/RTLABP912201_17719185_1_v1_2x.jpg', 'https://a.lmcdn.ru/img236x341/R/T/RTLABY543901_18046135_1_v1.jpg']
-let type = ['sneakers', 'sneakers', 'sneakers', 'sneakers', 'sneakers', 'sneakers', 'sneakers', 'sneakers', 'sneakers', 'sneakers', 'sneakers', 'sneakers', 'sneakers', 'sneakers', 'sneakers', 'sneakers', 'sneakers', 'sneakers', 'sneakers', 'sneakers']
-let name = ['adidas', 'adidas', 'adidas', 'adidas', 'adidas', 'adidas', 'adidas', 'adidas', 'adidas', 'adidas', 'adidas', 'adidas', 'adidas', 'adidas', 'adidas', 'adidas', 'adidas', 'adidas', 'adidas', 'adidas']
-let price = ['79$', '94$', '88$', '99$', '79$', '94$', '88$', '99$', '79$', '94$', '88$', '99$', '79$', '94$', '88$', '79$', '94$', '88$', '99$', '99$']
 
-price.forEach((v, i) => {
-    globalKart['sneakersInformation' + String(i + 1)] = {
-        image: image[i],
-        type: type[i],
-        name: name[i],
-        price: price[i],
-    }
-})
+const sneakersInfo = [
+    {
+        image: 'https://a.lmcdn.ru/img236x341/R/T/RTLABM929601_17356797_1_v1.jpg',
+        type: 'sneakers',
+        name: 'adidas',
+        price: '249$',
+    },
+    {
+        image: 'https://a.lmcdn.ru/img236x341/R/T/RTLABQ520101_17717054_1_v1.jpg',
+        type: 'sneakers',
+        name: 'adidas',
+        price: '153$',
+    },
+    {
+        image: 'https://a.lmcdn.ru/img236x341/A/D/AD093AUMOKG9_13279634_1_v1.jpg',
+        type: 'sneakers',
+        name: 'adidas',
+        price: '204$',
+    },
+    {
+        image: 'https://a.lmcdn.ru/img236x341/R/T/RTLACB638801_18413347_1_v1_2x.jpg',
+        type: 'sneakers',
+        name: 'adidas',
+        price: '95$',
+    },
+    {
+        image: 'https://a.lmcdn.ru/img236x341/R/T/RTLABX723401_17973506_1_v1.jpg',
+        type: 'sneakers',
+        name: 'adidas',
+        price: '249$',
+    },
+    {
+        image: 'https://a.lmcdn.ru/img236x341/R/T/RTLAAX263202_17963357_1_v1.jpg',
+        type: 'sneakers',
+        name: 'adidas',
+        price: '53$',
+    },
+    {
+        image: 'https://a.lmcdn.ru/img236x341/R/T/RTLABP405301_17739711_1_v1_2x.jpg',
+        type: 'sneakers',
+        name: 'adidas',
+        price: '389$',
+    },
+    {
+        image: 'https://a.lmcdn.ru/img236x341/R/T/RTLABX877701_17875917_1_v3.jpg',
+        type: 'sneakers',
+        name: 'adidas',
+        price: '249$',
+    },
+    {
+        image: 'https://a.lmcdn.ru/img236x341/R/T/RTLABK472901_17039311_1_v2.jpg',
+        type: 'sneakers',
+        name: 'adidas',
+        price: '454$',
+    },
+    {
+        image: 'https://a.lmcdn.ru/img236x341/R/T/RTLABY543901_18046135_1_v1.jpg',
+        type: 'sneakers',
+        name: 'adidas',
+        price: '359$',
+    },
+    {
+        image: 'https://a.lmcdn.ru/img236x341/R/T/RTLABP912201_17719185_1_v1_2x.jpg',
+        type: 'sneakers',
+        name: 'adidas',
+        price: '294$',
+    },
+    {
+        image: 'https://a.lmcdn.ru/img236x341/R/T/RTLACA047201_18082402_1_v1_2x.jpg',
+        type: 'sneakers',
+        name: 'adidas',
+        price: '193$',
+    },
+    {
+        image: 'https://a.lmcdn.ru/img236x341/R/T/RTLABM929601_17356797_1_v1.jpg',
+        type: 'sneakers',
+        name: 'adidas',
+        price: '153$',
+    },
+    {
+        image: 'https://a.lmcdn.ru/img236x341/R/T/RTLABQ520101_17717054_1_v1.jpg',
+        type: 'sneakers',
+        name: 'adidas',
+        price: '134$',
+    },
+    {
+        image: 'https://a.lmcdn.ru/img236x341/R/T/RTLAAX263202_17963357_1_v1.jpg',
+        type: 'sneakers',
+        name: 'adidas',
+        price: '174$',
+    },
+    {
+        image: 'https://a.lmcdn.ru/img236x341/R/T/RTLABX723401_17973506_1_v1.jpg',
+        type: 'sneakers',
+        name: 'adidas',
+        price: '73$',
+    },
+    {
+        image: 'https://a.lmcdn.ru/img236x341/A/D/AD093AUMOKG9_13279634_1_v1.jpg',
+        type: 'sneakers',
+        name: 'adidas',
+        price: '233$',
+    },
+    {
+        image: 'https://a.lmcdn.ru/img236x341/R/T/RTLABK472901_17039311_1_v2.jpg',
+        type: 'sneakers',
+        name: 'adidas',
+        price: '104$',
+    },
+    {
+        image: 'https://a.lmcdn.ru/img236x341/R/T/RTLABP912201_17719185_1_v1_2x.jpg',
+        type: 'sneakers',
+        name: 'adidas',
+        price: '254$',
+    },
+    {
+        image: 'https://a.lmcdn.ru/img236x341/R/T/RTLABY543901_18046135_1_v1.jpg',
+        type: 'sneakers',
+        name: 'adidas',
+        price: '149$',
+    },
+]
 
-let massAllSneakersInfo =  Object.values(globalKart)
 
-let coso = massAllSneakersInfo.forEach((sneaker) => {
+sneakersInfo.forEach((sneaker) => {
     const sneakersContainer = document.getElementById('sneakersContainer')
     const sneakerItem = document.createElement('div')
     const pictureSneakers = document.createElement('img')
@@ -68,35 +176,7 @@ let coso = massAllSneakersInfo.forEach((sneaker) => {
             heart = false
         }
     })
-
 })
-
-
-
-
-let subMenu = document.getElementById('submenu')
-
-submenu.style.display = 'none'
-subMenu.style.width = '152px'
-subMenu.style.boxShadow = '0px 8px 16px 0px rgba(0,0,0,0.2)'
-subMenu.style.backgroundColor = '#f9f9f9'
-subMenu.style.padding = '12px'
-subMenu.style.listStyleType = 'none'
-subMenu.style.position = 'absolute'
-
-
-const theSizeWrapper = document.getElementById('theSizeWrapper')
-
-theSizeWrapper.onmouseover = function(){
-    subMenu.style.display = 'block'
-    subMenu.style.marginTop = '0px'
-}
-
-theSizeWrapper.onmouseout = function(){
-    subMenu.style.display = 'none'
-}
-
-
 
 
 const submenuSort = document.getElementById('submenu-sort')
@@ -105,7 +185,6 @@ submenuSort.addEventListener('mouseover', (ev) => {
     let tar = ev.target.closest('li')
     tar.className = 'itemDropMenu'
     tar.style.backgroundColor = '#e9e9e9'
-    console.log(tar);
 })
 submenuSort.addEventListener('mouseout', (ev) => {
     let tar = ev.target.closest('li')
@@ -113,6 +192,9 @@ submenuSort.addEventListener('mouseout', (ev) => {
 })
 submenuSort.addEventListener('click', (ev) => {
     let tar = ev.target.textContent
-    alert(tar);
+    if( tar == 'По возростанию цены'){
+        console.log(sneakersInfo);
+    } else if(tar == 'По убыванию цены'){
+        console.log(sneakersInfo);
+    }
 })
-
