846d15f18c986faa0831340ec2d54246c6d832c4
DidarHalla <didarhallayev255@gmail.com>
2022-12-11 04:06:14 +0300

    corrected in the comparison condition from text to id. created a function that sorts by increase and decrease in the price of cards. and any small fixed

--- index.js
+++ index.js
@@ -78,7 +78,7 @@ const sneakersInfo = [
         image: 'https://a.lmcdn.ru/img236x341/R/T/RTLABM929601_17356797_1_v1.jpg',
         type: 'sneakers',
         name: 'adidas',
-        price: 15,
+        price: 165,
     },
     {
         image: 'https://a.lmcdn.ru/img236x341/R/T/RTLABQ520101_17717054_1_v1.jpg',
@@ -182,20 +182,20 @@ sneakersInfo.forEach((sneaker) => {
 
 const submenuSort = document.getElementById('submenu-sort')
 
-submenuSort.addEventListener('mouseover', (ev) => {
-    let tar = ev.target.closest('li')
+submenuSort.addEventListener('mouseover', (event) => {
+    let tar = event.target.closest('li')
     tar.className = 'itemDropMenu'
     tar.style.backgroundColor = '#e9e9e9'
 })
-submenuSort.addEventListener('mouseout', (ev) => {
-    let tar = ev.target.closest('li')
+submenuSort.addEventListener('mouseout', (event) => {
+    let tar = event.target.closest('li')
     tar.style.backgroundColor = ''
 })
 
 
 
-submenuSort.addEventListener('click', (ev) => {
-    let tar = ev.target
+submenuSort.addEventListener('click', (event) => {
+    let tar = event.target
 
     if( tar.id === 'sortByRisingPrice'){
         let get = document.getElementById('sneakersContainer')
@@ -208,8 +208,9 @@ submenuSort.addEventListener('click', (ev) => {
         let getContainer = document.getElementById('container')
         getContainer.append(sneakersContainer)
 
+        sneakersInfo.sort(function(a, b){return a.price - b.price})
 
-          sneakersInfo.forEach((sneaker) => {
+        sneakersInfo.forEach((sneaker) => {
             const sneakersContainer = document.getElementById('sneakersContainer')
             const sneakerItem = document.createElement('div')
             const pictureSneakers = document.createElement('img')
@@ -239,10 +240,6 @@ submenuSort.addEventListener('click', (ev) => {
             pictureRedHeart.src = 'red-heart.svg'
             pictureRedHeart.className = 'pictureRedHeart'
             
-            let changeType = Object.entries(sneaker)
-         
-            console.log(changeType.sort(function(a, b){return a - b}));
-
             sneakersPrice.append(sneaker.price + ' $')
             sneakersPrice.className = 'sneakersPrice'
         
@@ -268,6 +265,70 @@ submenuSort.addEventListener('click', (ev) => {
         })
                 
     } else if(tar.id === 'sortByDownwardPrice'){
-        console.log(sneakersInfo);
-    }
+        let get = document.getElementById('sneakersContainer')
+        get.remove()
+
+        let sneakersContainer = document.createElement('div')
+        sneakersContainer.id = 'sneakersContainer'
+        sneakersContainer.className = 'sneakersContainer'
+
+        let getContainer = document.getElementById('container')
+        getContainer.append(sneakersContainer)
+
+        sneakersInfo.sort(function(a, b){return b.price - a.price})
+
+        sneakersInfo.forEach((sneaker) => {
+            const sneakersContainer = document.getElementById('sneakersContainer')
+            const sneakerItem = document.createElement('div')
+            const pictureSneakers = document.createElement('img')
+            const pictureHeart = document.createElement('img')
+            const pictureRedHeart = document.createElement('img')
+            const sneakersPrice = document.createElement('p')
+            const sneakersName = document.createElement('p')
+            const sneakersType = document.createElement('p')
+        
+            sneakersContainer.append(sneakerItem)
+        
+            sneakerItem.className = 'sneakerItem'
+            sneakerItem.id = 'sneakerItem'
+            sneakerItem.append(pictureSneakers)
+            sneakerItem.append(pictureHeart)
+            sneakerItem.append(sneakersPrice)
+            sneakerItem.append(sneakersName)
+            sneakerItem.append(sneakersType)
+        
+            pictureSneakers.className = 'pictureSneakers'
+            pictureSneakers.alt = 'picture Sneakers'
+            pictureSneakers.src = sneaker.image
+        
+            pictureHeart.className = 'pictureHeart'
+            pictureHeart.src = 'icon-heart.svg'
+        
+            pictureRedHeart.src = 'red-heart.svg'
+            pictureRedHeart.className = 'pictureRedHeart'
+            
+            sneakersPrice.append(sneaker.price + ' $')
+            sneakersPrice.className = 'sneakersPrice'
+        
+            sneakersName.append(sneaker.name)
+            sneakersName.className = 'itemDescription'
+        
+            sneakersType.append(sneaker.type)
+            sneakersType.className = 'itemDescription'
+        
+            
+            let heart = false
+        
+            pictureHeart.addEventListener('click', function (event){
+                let tar = event.target
+                if(!heart){
+                    tar.src = 'red-heart.svg'
+                    heart = true
+                }else{
+                    tar.src = 'icon-heart.svg'
+                    heart = false
+                }
+            })
+        })
+        }
 })
