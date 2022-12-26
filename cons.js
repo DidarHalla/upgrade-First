cf928b37469d26362b8861d5651242eb1997b4aa
Didarhalla <didarhallayev255@gmail.com>
2022-12-26 20:44:27 +0300

    test

--- cons.js
+++ cons.js
@@ -7,119 +7,139 @@ export const sneakersInfo = [
         type: 'sneakers',
         name: 'adidas',
         price: 249,
+        id: 'sneaker-1'
     },
     {
         image: 'https://a.lmcdn.ru/img236x341/R/T/RTLABQ520101_17717054_1_v1.jpg',
         type: 'sneakers',
         name: 'adidas',
         price: 153,
+        id: 'sneaker-2'
     },
     {
         image: 'https://a.lmcdn.ru/img236x341/A/D/AD093AUMOKG9_13279634_1_v1.jpg',
         type: 'sneakers',
         name: 'adidas',
         price: 204,
+        id: 'sneaker-3'
     },
     {
         image: 'https://a.lmcdn.ru/img236x341/R/T/RTLACB638801_18413347_1_v1_2x.jpg',
         type: 'sneakers',
         name: 'adidas',
         price: 95,
+        id: 'sneaker-4'
     },
     {
         image: 'https://a.lmcdn.ru/img236x341/R/T/RTLABX723401_17973506_1_v1.jpg',
         type: 'sneakers',
         name: 'adidas',
         price: 249,
+        id: 'sneaker-5'
     },
     {
         image: 'https://a.lmcdn.ru/img236x341/R/T/RTLAAX263202_17963357_1_v1.jpg',
         type: 'sneakers',
         name: 'adidas',
         price: 53,
+        id: 'sneaker-6'
     },
     {
         image: 'https://a.lmcdn.ru/img236x341/R/T/RTLABP405301_17739711_1_v1_2x.jpg',
         type: 'sneakers',
         name: 'adidas',
         price: 389,
+        id: 'sneaker-7'
     },
     {
         image: 'https://a.lmcdn.ru/img236x341/R/T/RTLABX877701_17875917_1_v3.jpg',
         type: 'sneakers',
         name: 'adidas',
         price: 249,
+        id: 'sneaker-8'
     },
     {
         image: 'https://a.lmcdn.ru/img236x341/R/T/RTLABK472901_17039311_1_v2.jpg',
         type: 'sneakers',
         name: 'adidas',
         price: 454,
+        id: 'sneaker-9'
     },
     {
         image: 'https://a.lmcdn.ru/img236x341/R/T/RTLABY543901_18046135_1_v1.jpg',
         type: 'sneakers',
         name: 'adidas',
         price: 359,
+        id: 'sneaker-10'
     },
     {
         image: 'https://a.lmcdn.ru/img236x341/R/T/RTLABP912201_17719185_1_v1_2x.jpg',
         type: 'sneakers',
         name: 'adidas',
         price: 294,
+        id: 'sneaker-11'
     },
     {
         image: 'https://a.lmcdn.ru/img236x341/R/T/RTLACA047201_18082402_1_v1_2x.jpg',
         type: 'sneakers',
         name: 'adidas',
         price: 193,
+        id: 'sneaker-12'
     },
     {
         image: 'https://a.lmcdn.ru/img236x341/R/T/RTLABM929601_17356797_1_v1.jpg',
         type: 'sneakers',
         name: 'adidas',
         price: 165,
+        id: 'sneaker-13'
     },
     {
         image: 'https://a.lmcdn.ru/img236x341/R/T/RTLABQ520101_17717054_1_v1.jpg',
         type: 'sneakers',
         name: 'adidas',
         price: 134,
+        id: 'sneaker-14'
     },
     {
         image: 'https://a.lmcdn.ru/img236x341/R/T/RTLAAX263202_17963357_1_v1.jpg',
         type: 'sneakers',
         name: 'adidas',
         price: 174,
+        id: 'sneaker-15'
     },
     {
         image: 'https://a.lmcdn.ru/img236x341/R/T/RTLABX723401_17973506_1_v1.jpg',
         type: 'sneakers',
         name: 'adidas',
         price: 73,
+        id: 'sneaker-16'
     },
     {
         image: 'https://a.lmcdn.ru/img236x341/A/D/AD093AUMOKG9_13279634_1_v1.jpg',
         type: 'sneakers',
         name: 'adidas',
         price: 233,
+        id: 'sneaker-17'
     },
     {
         image: 'https://a.lmcdn.ru/img236x341/R/T/RTLABK472901_17039311_1_v2.jpg',
         type: 'sneakers',
         name: 'adidas',
         price: 104,
+        id: 'sneaker-18'
     },
     {
         image: 'https://a.lmcdn.ru/img236x341/R/T/RTLABP912201_17719185_1_v1_2x.jpg',
         type: 'sneakers',
         name: 'adidas',
         price: 254,
+        id: 'sneaker-19'
     },
     {
         image: 'https://a.lmcdn.ru/img236x341/R/T/RTLABY543901_18046135_1_v1.jpg',
         type: 'sneakers',
         name: 'adidas',
         price: 149,
+        id: 'sneaker-20'
     },
 ]
